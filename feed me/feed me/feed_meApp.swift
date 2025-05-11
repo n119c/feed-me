c92572219a5d971c56f7423dfb58fec258325ae8
n119c <119nvt@gmail.com>
2025-05-11 10:04:27 +1000

    Initial commit

---
+++ feed me/feed me/feed_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  feed_meApp.swift
+//  feed me
+//
+//  Created by Nick van Tilburg on 11/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct feed_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
